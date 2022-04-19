0ae0fd3a6f2464ed37057a770ed9321301e59c45
RonyC7 <79855001+RonyC7@users.noreply.github.com>
2022-04-18 23:17:30 -0600

    Agregar datos en razones financieras

--- Project2_Contability/Group Class/Statement.cs
+++ Project2_Contability/Group Class/Statement.cs
@@ -15,6 +15,8 @@ namespace Project2_Contability.Group_Class
 
         }
 
+        
+
         public List<StatementToShow> GenerarRazones(List<Account> BalanceDeSaldos)
         {
             List<StatementToShow> salir = new List<StatementToShow>();
@@ -23,11 +25,30 @@ namespace Project2_Contability.Group_Class
             salir.Add(new StatementToShow("", "", ""));
             salir.Add(new StatementToShow("", "RAZONES DE LIQUIDEZ", ""));
             salir.Add(new StatementToShow("", "", ""));
+            salir.Add(new StatementToShow("INDICE DE LIQUIDEZ", "", ""));
+            salir.Add(new StatementToShow("PRUEBA ACIDA", "", ""));
+            salir.Add(new StatementToShow("PRUEBA SEVERA", "", ""));
+            salir.Add(new StatementToShow("", "", ""));
             salir.Add(new StatementToShow("", "RAZONES DE ACTIVIDAD", ""));
+            salir.Add(new StatementToShow("", "", ""));
+            salir.Add(new StatementToShow("ROTACION DE INVENTARIOS", "", ""));
+            salir.Add(new StatementToShow("EDAD DE INVENTARIOS", "", ""));
+            salir.Add(new StatementToShow("PERIODO DE COBRO", "", ""));
+            salir.Add(new StatementToShow("ROTACION DE CITAS POR COBRAR", "", ""));
+            salir.Add(new StatementToShow("ROTACION DE ACTIVOS", "", ""));
             salir.Add(new StatementToShow("", "", ""));
             salir.Add(new StatementToShow("", "RAZONES DE SOLVENCIA Y ENDEUDAMIENTO", ""));
             salir.Add(new StatementToShow("", "", ""));
+            salir.Add(new StatementToShow("INDICE DE ENDEUDAMIENTO", "", ""));
+            salir.Add(new StatementToShow("ESTRUCTURA DE CAPITAL", "", ""));
+            salir.Add(new StatementToShow("CARGO DE INTERESES", "", ""));
+            salir.Add(new StatementToShow("", "", ""));
             salir.Add(new StatementToShow("", "RAZONES DE RENTABILIDAD", ""));
+            salir.Add(new StatementToShow("", "", ""));
+            salir.Add(new StatementToShow("RENTABILIDAD SOBRE VENTAS", "", ""));
+            salir.Add(new StatementToShow("RENTABILIDAD SOBRE ACTIVOS FIJOS", "", ""));
+            salir.Add(new StatementToShow("RENTABILIDAD SOBRE CAPITAL INICIAL", "", ""));
+            salir.Add(new StatementToShow("RENTABILIDAD SOBRE CAPITAL CONTABLE", "", ""));
 
             GeneralBalance gb = new GeneralBalance();
             EstadoResultados er = new EstadoResultados();
